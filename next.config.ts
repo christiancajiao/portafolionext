a42db8d5aff3809b757903d0c216d0f53d50a50b
christiancajiao <christiancamilocajiaochacon@gmail.com>
2026-02-26 22:01:28 -0500

    fix 3

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // @ts-expect-error - turbopack root is validconfig but missing from types
-    turbopack: {
-      root: process.cwd(),
-    },
-  },
+  output: "export",
 };
 
 export default nextConfig;
